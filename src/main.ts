2de0b728feb4297c534c64ee8fde9cf2a310ff80
luis10barbo <luis10barbo2020@gmail.com>
2025-02-02 14:54:50 -0300

    Adicionado scripts de run com typescript

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+import {Client, GatewayIntentBits} from "discord.js";
+const client = new Client({
+    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", (message) => {
+    console.log("mensagem")
+    if (message.author.bot) return;
+    if (message.content == "!penis") {
+        message.channel.send("amo penis");
+    }
+});
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
